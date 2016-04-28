00757a4bd12fe6e1f89403db5eaa112671a6001c
Carl Green <carlgreen@gmail.com>
2016-04-28 18:08:20 +1200

    use a struct to hold colors

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use image::{
     ImageBuffer
 };
 
+struct Color {
+    r: f64,
+    g: f64,
+    b: f64,
+}
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -19,12 +25,14 @@ fn main() {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r = i as f32 / nx as f32;
-            let g = j as f32 / ny as f32;
-            let b = 0.2;
-            let ir = (255.99 * r) as u8;
-            let ig = (255.99 * g) as u8;
-            let ib = (255.99 * b) as u8;
+            let col = Color {
+                r: i as f64 / nx as f64,
+                g: j as f64 / ny as f64,
+                b: 0.2,
+            };
+            let ir = (255.99 * col.r) as u8;
+            let ig = (255.99 * col.g) as u8;
+            let ib = (255.99 * col.b) as u8;
             let pixel = image::Rgb([ir, ig, ib]);
             img.put_pixel(i, j, pixel);
         }
