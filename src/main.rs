9cf2af9088e118d013483172c12669b3cb8fa800
Carl Green <carlgreen@gmail.com>
2016-04-28 17:51:07 +1200

    create simple image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+use image::{
+    GenericImage,
+    ImageBuffer
+};
+
 fn main() {
-    println!("Hello, world!");
+    let nx = 200;
+    let ny = 100;
+
+    let mut img = ImageBuffer::new(nx, ny);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = i as f32 / nx as f32;
+            let g = j as f32 / ny as f32;
+            let b = 0.2;
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+            let pixel = image::Rgb([ir, ig, ib]);
+            img.put_pixel(i, j, pixel);
+        }
+    }
+
+    let ref mut fout = File::create(&Path::new("target/image.png")).unwrap();
+    let _ = image::ImageRgb8(img).save(fout, image::PNG);
 }
