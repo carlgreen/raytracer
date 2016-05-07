7c6620d46f59f76d65e0908a30caea4b66f65144
Carl Green <carlgreen@gmail.com>
2016-05-07 14:21:37 +1200

    add test for ray point_at_parameter

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,6 @@
 use vector::Vector;
 
+#[derive(Debug)]
 pub struct Ray {
     a: Vector,
     b: Vector,
@@ -27,5 +28,27 @@ impl Ray {
             y: self.a.y + t * self.b.y,
             z: self.a.z + t * self.b.z,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Ray;
+    use vector::Vector;
+
+    #[test]
+    fn test_point_at_parameter() {
+        let ray = Ray::new(&Vector {
+                               x: 0.1,
+                               y: 0.1,
+                               z: 0.1,
+                           },
+                           &Vector::new_ones_vector());
+        assert_eq!(ray.point_at_parameter(0.5),
+                   Vector {
+                       x: 0.6,
+                       y: 0.6,
+                       z: 0.6,
+                   });
     }
 }
